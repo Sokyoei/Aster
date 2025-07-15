c8d7c665eb66b3c82321a139dc6b5f633ae446da
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2025-07-15 16:57:16 +0800

    add msbuild project for Aster

---
+++ src/i18n/Aster_zh_CN.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="28"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="34"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="39"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="49"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,28 @@
 #include <QVBoxLayout>
 #include <QWidget>
 
+#include "src/mainwindow.hpp"
+
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
-    // 创建主窗口部件
-    QWidget window;
+    // // 创建主窗口部件
+    // QWidget window;
+    // // 创建 QCamera 对象，使用默认摄像头
+    // QCamera* camera = new QCamera(QCameraInfo::defaultCamera(), &window);
+    // // 创建 QCameraViewfinder 对象
+    // QCameraViewfinder* viewfinder = new QCameraViewfinder(&window);
+    // // 设置 QCamera 的视图取景器
+    // camera->setViewfinder(viewfinder);
+    // // 创建垂直布局
+    // QVBoxLayout* layout = new QVBoxLayout(&window);
+    // layout->addWidget(viewfinder);
+    // // 启动摄像头
+    // camera->start();
+    // // 显示主窗口
+    // window.show();
 
-    // 创建 QCamera 对象，使用默认摄像头
-    QCamera* camera = new QCamera(QCameraInfo::defaultCamera(), &window);
-
-    // 创建 QCameraViewfinder 对象
-    QCameraViewfinder* viewfinder = new QCameraViewfinder(&window);
-
-    // 设置 QCamera 的视图取景器
-    camera->setViewfinder(viewfinder);
-
-    // 创建垂直布局
-    QVBoxLayout* layout = new QVBoxLayout(&window);
-    layout->addWidget(viewfinder);
-
-    // 启动摄像头
-    camera->start();
-
-    // 显示主窗口
+    MainWindow window;
     window.show();
 
     return app.exec();
